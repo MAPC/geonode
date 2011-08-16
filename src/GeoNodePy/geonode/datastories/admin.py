b6bc7e92fba083c268fe119dff423e63cc18b1c0
Christian Spanring <cspanring@gmail.com>
2011-08-16 13:05:29 -0400

    Updated Datastory models

--- src/GeoNodePy/geonode/datastories/admin.py
+++ src/GeoNodePy/geonode/datastories/admin.py
@@ -1,7 +1,37 @@
 from django.contrib import admin
-from geonode.datastories.models import Datastory, Page, Visualizationpage, Mappage
+from geonode.datastories.models import Datastory, Page, Visualizationpage, Mappage, Storypage, Textpage
 
-admin.site.register(Datastory, admin.ModelAdmin)
-admin.site.register(Page, admin.ModelAdmin)
-admin.site.register(Visualizationpage, admin.ModelAdmin)
-admin.site.register(Mappage, admin.ModelAdmin)
+
+class DatastoryAdmin(admin.ModelAdmin):
+	list_display = ('title', 'owner',)
+	exclude = ('owner',)
+	prepopulated_fields = {'slug': ('title',)}
+	search_fields = ('title', 'abstract',)
+	
+	def save_model(self, request, obj, form, change):
+		if getattr(obj, 'owner', None) is None:
+			obj.owner = request.user
+		obj.save()
+
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title', 'owner',)
+	exclude = ('owner',)
+	
+	def save_model(self, request, obj, form, change):
+		if getattr(obj, 'owner', None) is None:
+			obj.owner = request.user
+		obj.save()
+	
+
+class StorypageAdmin(admin.ModelAdmin):
+	list_display = ('pk', 'datastory', 'page', 'pageorder')
+	list_editable= ('pageorder',)
+	list_display_links = ('pk',)
+	ordering = ['datastory', 'pageorder']
+
+admin.site.register(Datastory, DatastoryAdmin)
+admin.site.register(Storypage, StorypageAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(Visualizationpage, PageAdmin)
+admin.site.register(Mappage, PageAdmin)
+admin.site.register(Textpage, PageAdmin)
--- src/GeoNodePy/geonode/datastories/models.py
+++ src/GeoNodePy/geonode/datastories/models.py
@@ -3,54 +3,37 @@ from django.utils.translation import ugettext as _
 from django.db import models
 
 from django.contrib.auth.models import User
-from model_utils.managers import InheritanceManager
 
-# Create your models here.
-class Datastory(models.Model):
-	"""
-	Model to store a data story, a step-by-step walk through data exploration.
-	"""
-	title = models.CharField(_('Title'), max_length=100)
-	slug  = models.SlugField(max_length=100)
-	abstract = models.TextField(_('Abstract'), blank=True, null=True)
-	
-	# FIXME: allow ordering of pages
-	pages = models.ManyToManyField('Page')
-	
-	owner = models.ForeignKey(User, verbose_name=_('owner'))
-	
-	last_modified = models.DateTimeField(auto_now_add=True)
-	
-	def __unicode__(self):
-		return '%s by %s' % (self.title, (self.owner.username if self.owner else "<Anonymous>"))
+from geonode.maps.models import Map
+from geonode.weave.models import Visualization
 		
-	class Meta:
-		unique_together = (('title', 'owner'),)
-		verbose_name_plural = "Datastories"
 		
 class Page(models.Model):
+	""" 
+	The is the parent class for all possible page types.
 	"""
-	A page is part of a data story.
-	Default page is a Textpage.
-	"""
+	
 	title = models.CharField(_('Title'), max_length=100)
-	abstract = models.TextField(_('Abstract'), blank=True, null=True)
-	
-	owner = models.ForeignKey(User, verbose_name=_('owner'))
-	
+	owner = models.ForeignKey(User, verbose_name=_('owner'), blank=True, null=True)
 	last_modified = models.DateTimeField(auto_now_add=True)
-	
-	objects = InheritanceManager()
 	
 	def __unicode__(self):
 		return '%s by %s' % (self.title, (self.owner.username if self.owner else "<Anonymous>"))
+
+class Textpage(Page):
+	"""
+	A page showing text only.
+	"""
 	
+	abstract = models.TextField(_('Abstract'), blank=True, null=True)
 		
 class Visualizationpage(Page):
 	"""
 	A page showing a Weave visualization.
 	"""
-	visualization = models.ForeignKey('weave.Visualization')
+	
+	caption = models.TextField(_('Caption'), blank=True, null=True)
+	visualization = models.ForeignKey(Visualization)
 	
 	@models.permalink
 	def get_absolute_url(self):
@@ -60,8 +43,42 @@ class Mappage(Page):
 	"""
 	A page showing a Map.
 	"""
-	map = models.ForeignKey('maps.Map')
+	
+	caption = models.TextField(_('Caption'), blank=True, null=True)
+	map = models.ForeignKey(Map)
 	
 	@models.permalink
 	def get_absolute_url(self):
 		return ('geonode.maps.views.view', None, { 'mapid': self.map.id, })
+
+
+class Datastory(models.Model):
+	"""
+	Model to store a data story, a step-by-step walk through data exploration.
+	"""
+
+	title = models.CharField(_('Title'), max_length=100)
+	slug  = models.SlugField(max_length=100)
+	abstract = models.TextField(_('Abstract'), blank=True, null=True)
+
+	owner = models.ForeignKey(User, verbose_name=_('owner'), blank=True, null=True)
+	last_modified = models.DateTimeField(auto_now_add=True)
+
+	datastory = models.ManyToManyField(Page, through='Storypage')
+
+	def __unicode__(self):
+		return '%s by %s' % (self.title, (self.owner.username if self.owner else "<Anonymous>"))
+
+	class Meta:
+		unique_together = (('title', 'owner'),)
+		verbose_name_plural = "Datastories"
+
+class Storypage(models.Model):
+	"""
+	Amending the ManyToMany Datastory-Page relation with additional information.
+	"""
+	
+	datastory = models.ForeignKey(Datastory)
+	page = models.ForeignKey(Page)
+	pageorder = models.IntegerField('Order')
+
