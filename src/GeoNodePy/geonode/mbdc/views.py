20603af2436339efb9327a1aded84136b48435c2
Christian Spanring <cspanring@gmail.com>
2011-11-03 19:38:03 -0400

    added visualization gallery teaser on homepage
    …and adjusted featured visualization query to use permission utility.

--- src/GeoNodePy/geonode/mbdc/views.py
+++ src/GeoNodePy/geonode/mbdc/views.py
@@ -2,6 +2,8 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from geonode.mbdc.models import Hero, Featured
+from geonode.weave.models import Visualization
+from geonode.weave.utils import get_readable_vis
 
 
 def index(request): 
@@ -9,6 +11,13 @@ def index(request):
 
 	hero_entries = Hero.objects.filter(active=True)
 
-	featured_visualizations = Featured.objects.all()
+	# get set of visualizations viewable by current user
+	readable_vis = get_readable_vis(request.user)
+	
+	# featured visualizations
+	featured_visualizations = Featured.objects.filter(visualization__in=readable_vis)
+
+	# get latest 10 visualizations for gallery
+	gallery_visualizations = Visualization.objects.filter(id__in=readable_vis).order_by('-last_modified')[:10]
 
 	return render_to_response('mbdc/index.html', locals(), context_instance=RequestContext(request))
