0a2e4e840c3248589acb835c3f126f00de9d9e86
Christian Spanring <cspanring@gmail.com>
2011-11-02 20:47:14 -0400

    added featured visualizations to homepage.
    including functionality to turn a thumbnail image into an interactive
    weave instance.

--- src/GeoNodePy/geonode/mbdc/views.py
+++ src/GeoNodePy/geonode/mbdc/views.py
@@ -1,12 +1,14 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-from geonode.mbdc.models import Hero
+from geonode.mbdc.models import Hero, Featured
 
 
 def index(request): 
 	""" Renders homepage """
 
 	hero_entries = Hero.objects.filter(active=True)
+
+	featured_visualizations = Featured.objects.all()
 
 	return render_to_response('mbdc/index.html', locals(), context_instance=RequestContext(request))
